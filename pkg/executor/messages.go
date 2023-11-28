c5aeb816055d51c2abe978b22203ff6567158a99
Alexey Efremov <amefremov@edu.hse.ru>
2023-11-28 23:46:14 +0300

    minor fixes

--- pkg/executor/messages.go
+++ pkg/executor/messages.go
@@ -11,11 +11,11 @@ const (
 )
 
 func GetTurnLightOffMessage(keyCode int) midi.Message {
-	msg := midi.Message{byte(NoteOffCode + 1), byte(keyCode), byte(0)}
+	msg := midi.Message{byte(NoteOffCode), byte(keyCode), byte(0)}
 	return msg
 }
 
 func GetTurnLightOnMessage(keyCode int) midi.Message {
-	msg := midi.Message{byte(NoteOnCode + 1), byte(keyCode), byte(2)}
+	msg := midi.Message{byte(NoteOnCode), byte(keyCode), byte(0)}
 	return msg
 }
